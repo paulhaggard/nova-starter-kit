1363c01d53b69175346e1536804216b9c593e842
paulhaggard <paulhaggard@gmail.com>
2018-02-01 20:25:38 -0600

    recomment LED line

--- 07_bdcTemp/main.py
+++ 07_bdcTemp/main.py
@@ -69,6 +69,6 @@ try:
                 print 'Error descriptions: https://hologram.io/docs/reference/cloud/python-sdk/#-sendmessage-message-topics-none-timeout-5-'
 
 finally:
-    GPIO.output(LED_PIN,False) ## Switch off LED
+    #GPIO.output(LED_PIN,False) ## Switch off LED
     GPIO.cleanup()  ## reset all pins
     hologram.network.disconnect() ## Exercise 06 - disconnect from the cellular network
